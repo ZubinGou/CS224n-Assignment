ec19856f271d88ba022066517ce01a2f57cfc12a
ZubinGou <zebgou@gmail.com>
2021-01-29 17:06:56 +0800

    a3

--- a3/run.py
+++ a3/run.py
@@ -134,8 +134,8 @@ def train_for_epoch(parser, train_data, dev_data, optimizer, loss_func, batch_si
 
 if __name__ == "__main__":
     # Note: Set debug to False, when training on entire corpus
+    debug = False
     # debug = True
-    debug = True
 
     # assert(torch.__version__ == "1.0.0"),  "Please install torch version 1.0.0"
 
